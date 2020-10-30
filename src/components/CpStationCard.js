c8f66ae6882e7dcd50937739c88f5dde9c3940dd
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-10-30 00:56:47 -0400

    finished Christopher

--- src/components/CpStationCard.js
+++ src/components/CpStationCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-// import StationName from './stations/HbStationName'
-// import CurrentCard from './current/HbCurrentCard'
+import StationName from './stations/ApStationName'
+import CurrentCard from './current/CpCurrentCard'
 import CurrentTime from './current/CurrentTime'
 
 function StationCard() {
@@ -9,15 +9,17 @@ function StationCard() {
     return (
       <>
       <div className='station'>
+        <h2>Christopher's Weather</h2>
+        <h3>Or Close by</h3>
         <div className='data'>
-          {/* <StationName /> */}
+          <StationName />
         </div>
         <div className='current'>
           <CurrentTime />
         </div>
         <div className='conditions'>
           <h3>Current Conditions</h3>
-            {/* <CurrentCard /> */}
+            <CurrentCard />
         </div>
       </div>
       </>
